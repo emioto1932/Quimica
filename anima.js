5bb6184c8a996a9c43cac5922153a1a9a3acda1b
emioto1932 <144555667+emioto1932@users.noreply.github.com>
2024-12-15 03:08:48 +0000

    Update anima.js

--- anima.js
+++ anima.js
@@ -885,7 +885,7 @@ elementSelect.addEventListener("change", () => {
 
     <tr><td>Raio atômico (pm  ou picômetros)</td><td>${elementoAtual.raioAtomico}</td><td>1ª Energia de ionização (kJ/mol)</td><td>${exibirComVirgula(elementoAtual.energiaIonizacao)}</td></tr> 
  
-<tr><td>Configuração Eletrônica (K L M N O P Q)</td><td>${elementoAtual.configuracaoEletronica}</td><td><a href="objeto.html?propriedade=densidade&grupo=${grupoSelecionado}&elemento=${elementoAtual.nome}" class="popup-link">Abrir Tabela de Densidade</a></td><td>${exibirComVirgula(elementoAtual.densidade)}</td></tr> 
+<tr><td>Configuração Eletrônica (K L M N O P Q)</td><td>${elementoAtual.configuracaoEletronica}</td><td><a href="objeto.html?propriedade=densidade&grupo=${encodeURIComponent(grupoSelecionado)}&elemento=${encodeURIComponent(elementoAtual.nome)}">Abrir Tabela de Densidade</a></td><td>${exibirComVirgula(elementoAtual.densidade)}</td></tr> 
 
   `; 
 
