b39cda980e429cfec95afcc94fc4b8631c110943
emioto1932 <144555667+emioto1932@users.noreply.github.com>
2024-12-15 02:39:53 +0000

    Update objeto.js

--- objeto.js
+++ objeto.js
@@ -1,11 +1,14 @@
-
 document.addEventListener("DOMContentLoaded", function() {
-    // Pegando o parâmetro da URL
+    // Pegando os parâmetros da URL
     const urlParams = new URLSearchParams(window.location.search);
     const propriedade = urlParams.get("propriedade");
+    const grupo = urlParams.get("grupo"); // Recupera o grupo
+    const elemento = urlParams.get("elemento"); // Recupera o elemento químico
 
     // Exibindo o nome da propriedade na pop-up
     const propertyNameElement = document.getElementById("propertyName");
+    const groupElement = document.getElementById("groupName"); // Elemento para exibir o grupo
+    const elementNameElement = document.getElementById("elementName"); // Elemento para exibir o nome do elemento
 
     if (propriedade) {
         // Definindo o nome da propriedade de acordo com o link clicado
@@ -16,6 +19,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     } else {
         propertyNameElement.textContent = "Propriedade Não Informada";
+    }
+
+    // Exibindo o grupo e o nome do elemento
+    if (grupo && elemento) {
+        groupElement.textContent = grupo; // Exibe o grupo
+        elementNameElement.textContent = elemento; // Exibe o nome do elemento
+    } else {
+        groupElement.textContent = "Grupo Não Informado";
+        elementNameElement.textContent = "Elemento Não Informado";
     }
 
     // Função para abrir a pop-up
