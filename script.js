a28eb21aa74685ea7051836f429f1075c202b4d5
emioto1932 <144555667+emioto1932@users.noreply.github.com>
2024-12-16 01:54:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,135 +1,69 @@
-// Selecionando o menu e o botão do menu  
+
+// Selecionando o menu e o botão do menu 
+
+const menuButton = document.getElementById('menu-button'); 
+
+const menu = document.getElementById('menu'); 
+
+const contentContainer = document.getElementById('content-container'); 
+
+const topics = document.querySelectorAll('a[data-topic]'); 
 
   
 
-const menuButton = document.getElementById('menu-button');  
+// Função para mostrar/esconder o menu 
+
+menuButton.addEventListener('click', () => { 
+
+    menu.classList.toggle('hidden'); 
+
+}); 
 
   
 
-const menu = document.getElementById('menu');  
+// Função para mostrar o conteúdo correto 
+
+topics.forEach(topic => { 
+
+    topic.addEventListener('click', (event) => { 
+
+        event.preventDefault(); 
+
+        const selectedTopic = event.target.getAttribute('data-topic'); 
+
+         
+
+    // Atualiza o título
+        document.getElementById('selected-topic').innerText = selectedTopic.replace(/-/g, ' ').toUpperCase();
 
   
 
-const contentContainer = document.getElementById('content-container');  
+  
 
   
 
-const topics = document.querySelectorAll('a[data-topic]');  
+         
+
+        // Esconde todos os conteúdos 
+
+        document.querySelectorAll('.content').forEach(content => { 
+
+            content.classList.remove('active'); 
+
+        }); 
+
+         
+
+        // Mostra o conteúdo do tópico selecionado 
+
+        document.getElementById(selectedTopic).classList.add('active'); 
 
   
 
-   
+        // Esconde o menu 
 
-  
+        menu.classList.add('hidden'); 
 
-// Função para mostrar/esconder o menu  
-
-  
-
-menuButton.addEventListener('click', () => {  
-
-  
-
-    menu.classList.toggle('hidden');  
-
-  
-
-});  
-
-  
-
-   
-
-  
-
-// Função para mostrar o conteúdo correto  
-
-  
-
-topics.forEach(topic => {  
-
-  
-
-    topic.addEventListener('click', (event) => {  
-
-  
-
-        event.preventDefault();  
-
-  
-
-        const selectedTopic = event.target.getAttribute('data-topic');  
-
-  
-
-          
-
-  
-
-    // Atualiza o título 
-
-        document.getElementById('selected-topic').innerText = selectedTopic.replace(/-/g, ' ').toUpperCase(); 
-
-  
-
-   
-
-  
-
-   
-
-  
-
-   
-
-  
-
-          
-
-  
-
-        // Esconde todos os conteúdos  
-
-  
-
-        document.querySelectorAll('.content').forEach(content => {  
-
-  
-
-            content.classList.remove('active');  
-
-  
-
-        });  
-
-  
-
-          
-
-  
-
-        // Mostra o conteúdo do tópico selecionado  
-
-  
-
-        document.getElementById(selectedTopic).classList.add('active');  
-
-  
-
-   
-
-  
-
-        // Esconde o menu  
-
-  
-
-        menu.classList.add('hidden');  
-
-  
-
-    });  
-
-  
+    }); 
 
 }); 
