57fda68030b05b2d986fdc16ac1fbe50c0052678
emioto1932 <144555667+emioto1932@users.noreply.github.com>
2024-12-22 23:23:56 -0300

    Update anima.js

--- anima.js
+++ anima.js
@@ -891,7 +891,7 @@ elementSelect.addEventListener("change", () => {
    <td> <a href="objeto.html?propriedade=densidade">Densidade (g/cm³)</a></td><td>${exibirComVirgula(elementoAtual.densidade)}</td></tr> 
 
 // Salvar no localStorage
-localStorage.setItem("numProtons", ${elementoAtual.protons);
+localStorage.setItem("numProtons", ${elementoAtual.protons});
 localStorage.setItem("numNeutrons", ${elementoAtual.numeroNeutrons});
   
  
