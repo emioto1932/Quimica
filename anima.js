fe5d18e2be8a9c7ecb5252aa4aab27dd43783a2a
emioto1932 <144555667+emioto1932@users.noreply.github.com>
2024-12-23 23:48:39 -0300

    Update anima.js

--- anima.js
+++ anima.js
@@ -878,10 +878,7 @@ elementSelect.addEventListener("change", () => {
 
   tableBody.innerHTML = `
 
-    <tr><td>
-
-    
-   <a href="numero-atomico.html?numProtons=" + ${elementoAtual.protons} + "&numNeutrons=" + ${elementoAtual.numeroNeutrons}>Número atômico ou Número de Prótons (carga +)</a></td><td id="numProtons">${elementoAtual.protons}</td><td>Categoria</td><td>${elementoAtual.categoria}</td></tr> 
+    <tr><td> <a href="numero-atomico.html?numProtons=" + ${elementoAtual.protons} + "&numNeutrons=" + ${elementoAtual.numeroNeutrons}>Número atômico ou Número de Prótons (carga +)</a></td><td id="numProtons">${elementoAtual.protons}</td><td>Categoria</td><td>${elementoAtual.categoria}</td></tr> 
 
     <tr><td>Número de Elétrons (carga -)</td><td>${elementoAtual.electrons}</td><td>Eletronegatividade (escala de Pauling)</td><td>${exibirComVirgula(elementoAtual.electronegativity)}</td></tr> 
 
